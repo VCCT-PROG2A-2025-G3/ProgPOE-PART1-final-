f9c6b9c639142047774a0a0ec8fc7b80bebb8999
brettJms <brettjames43@gmail.com>
2025-04-23 14:12:13 +0200

    Add project files.

---
+++ ProgPOE-PART1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgPOE_PART1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
