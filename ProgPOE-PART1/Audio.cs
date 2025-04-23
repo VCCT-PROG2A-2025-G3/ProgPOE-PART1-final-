d71de3da9fd54aa9b8813fbe4d77f3a325adbe4a
brettJms <brettjames43@gmail.com>
2025-04-23 14:21:44 +0200

    Audio is working correctly, ASCII art is complete. The programs opening stage is complete now

---
+++ ProgPOE-PART1/Audio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgPOE_PART1
+{
+    class Audio
+    {
+        private readonly string _filePath;
+
+        public Audio(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Play()
+        {
+            try
+            {
+                // Use 'using' so SoundPlayer disposes automatically
+                using (SoundPlayer player = new SoundPlayer(_filePath))
+                {
+                    player.PlaySync(); // Blocks until file finishes playing
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error playing WAV file: {ex.Message}");
+            }
+        }
+    }
+}
